5bdfb00ca21ee9171c05026c051f3d17ab7ffb54
Rifqi Akmal Saputra <72428679+Rifqiakmals12@users.noreply.github.com>
2021-01-25 18:39:26 +0700

    Delete Modul 4 directory

--- Modul 4/src/com/RifqiAS/Modul4/Interface/ChildCD.java
+++
@@ -1,8 +0,0 @@
-package com.RifqiAS.Modul4.Interface;
-
-public class ChildCD extends CD implements InterfaceCD {
-    // Constructor yang memiliki parameter ukuranCD dan hargaCD
-    public ChildCD(String ukuranCD, long hargaCD) {
-        super(ukuranCD, hargaCD);
-    }
-}
