ee85e3fa3cb13cf1474b3839905fc495f5f72714
Rifqi Akmal Saputra <72428679+Rifqiakmals12@users.noreply.github.com>
2021-01-25 18:47:49 +0700

    Delete Modul 2 directory

--- Modul 2/src/com/RifqiAS/Modul2/Overriding/X.java
+++
@@ -1,9 +0,0 @@
-package com.RifqiAS.Modul2.Overriding;
-
-// Class X
-public class X {
-    // Method getValue
-    public void getValue(String value){
-        System.out.println("Value class X\t: " + value);
-    }
-}
